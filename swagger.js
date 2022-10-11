eac7dc94b76f23b92cd5995996cd438e1a8aa62b
Ibraim Calvo <ibraim_10@hotmail.com>
2022-10-10 21:43:30 -0500

    documentation

--- swagger.js
+++ swagger.js
@@ -6,8 +6,8 @@ const doc = {
         title: 'Users API',
         description: 'Is an API of Users',
     },
-    host: 'cse341-project-b0xp.onrender.com',
-    schemes: ['https'],
+    host: 'localhost:3000',
+    schemes: ['http'],
 };
 
 const outputFile = './swagger.json';
